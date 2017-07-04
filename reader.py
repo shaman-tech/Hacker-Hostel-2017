2cc840f66af289626b09f81553888ffc477afdfa
Peter Gordon <kaktusvisions@gmail.com>
2017-07-04 00:48:53 -0500

    update sqlite database with table TGG_MERCHANDISE check schema. Changed Fields to a string of the table columns in db.sqlite

--- reader.py
+++ reader.py
@@ -1,5 +1,6 @@
 import pandas as pd
-import numpy as np 
+import numpy as np
+import sqlite3
 
 def create_sql_command(row_entry):
 	index = ""
@@ -21,19 +22,26 @@ def change_float_int(df_column):
 		df_column[index] = int(value)
 	return df_column
 
-
+cn = sqlite3.connect('db.sqlite')
+curs = cn.cursor()
 df = pd.read_csv('TGG_Merchandise.csv',skiprows=4)
 df = df.fillna(0)
 df['PO #']= df['PO #'].astype(int)
+#changedf['PO #']
 # scaler = lambda x: int(x)
 # df['PO #'].apply(scaler)
 column_names = list(df.columns)
 column_values  = df.values
-fields = create_sql_command(column_names)
+fields = "PONUM, PODate,SUPPLIER, ProductDescription, Qty, SizeContainer,ContainerNUM, ShippingLine,ETAJamaica,ETATGG,AADTGG,DateOffloaded,Dateleft"#create_sql_command(column_names)
+
 for row in column_values:
-	create_table_command = "{} TTG_MERCHANDISE ({}) {} ({})".format('INSERT INTO',fields,'VALUES',create_sql_command(row))
-	print("'%s'"%create_table_command)
-	print("")
+	create_table_command = "{} TGG_MERCHANDISE ({}) {} ({})".format('INSERT INTO',fields,'VALUES',create_sql_command(row))
+	curs.execute((create_table_command))
+
+
+
+
+
 
 # for name in column_names:
 # 	print(name.split())
